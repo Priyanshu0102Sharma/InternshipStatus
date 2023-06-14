a79c31afe14b58f52575332e5f0411e8f85f99be
Priyanshu <priyanshu0102here@gmail.com>
2023-06-14 18:48:16 +0530

    Updated

--- server/app.js
+++ server/app.js
@@ -7,11 +7,7 @@ const router = require("./Routes/router");
 const PORT = process.env.PORT || 6010
 
 
-app.use(cors({
-    origin:["https://internship-status.vercel.app/"],
-    methods:{"POST", "GET"},
-    credentials:true
-}));
+app.use(cors());
 app.use(express.json());
 app.use("/uploads",express.static("./uploads"));
 app.use("/files",express.static("./public/files"));
