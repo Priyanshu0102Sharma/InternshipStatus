5ce13d733352c18621af530c478af187a790a06d
Priyanshu <priyanshu0102here@gmail.com>
2023-06-14 17:28:57 +0530

    Modified

--- server/app.js
+++ server/app.js
@@ -14,6 +14,21 @@ app.use("/files",express.static("./public/files"));
 
 app.use(router);
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+// Your code
+
+
 app.listen(PORT,()=>{
     console.log(`Server start at port no ${PORT}`)
 })
